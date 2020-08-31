5abd539799ce36df9ad8ca3180cecf94ff21a59d
Kasake <locodediscord@gmail.com>
2020-08-31 23:26:36 +0200

    :3

--- src/CMDS/cosasde.js
+++ src/CMDS/cosasde.js
@@ -4,6 +4,7 @@ module.exports = {
     nombre: "pene",
     alias: [],
     run: (client, message, args) => {
+
         function Clave() {
             let clave = Math.random().toString(36).slice(6).split('').map((x, i) => x[(i % 2) ? 'toLowerCase' : 'toUpperCase']()).join('')
             return clave
@@ -14,7 +15,7 @@ module.exports = {
             const filter = m => m.content.includes(clave)
             const collector = message.channel.createMessageCollector(filter, { time: 15000 })
             
-            message.channel.send(Clave)
+            message.channel.send(clave)
             
             collector.on('collect', m => {
             message.channel.send('Correcto')
