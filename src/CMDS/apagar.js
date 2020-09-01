3ba4c6f20b1d6a88015427b806ae1b8e58f04716
Kasake <locodediscord@gmail.com>
2020-09-01 04:07:43 +0200

    :3

---
+++ src/CMDS/apagar.js
@@ -0,0 +1,27 @@
+const { Kasake, Melphi, ZorGame } = require('../../util/devs')
+
+
+require('../../util/devs')
+
+module.exports = {
+    run: async (client, message, args) => {
+ 
+        if(![Kasake, Melphi, ZorGame].includes(message.author.id)) return
+
+        try{
+            await message.channel.send('`DESCONECTADO`').then (() => {
+                    process.exit();
+                });
+            
+        
+
+} catch (error) {
+            message.channel.send('ERROR: '+ error.message)
+        }
+
+    },
+
+    nombre: "apagar",
+    alias: [""]
+
+}
---
+++ src/CMDS/reload.js
@@ -0,0 +1,11 @@
+const { Kasake, Melphi, ZorGame } = require('../../util/devs')
+
+require('../../util/devs')
+module.exports
+   run: async (client, message, args) => {
+  
+    if(![Kasake, Melphi, ZorGame].includes(message.author.id)) return
+
+   }
+
+   
