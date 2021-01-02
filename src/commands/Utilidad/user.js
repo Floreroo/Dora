ec780e44da177e990384b58f78ff7b4a2aa0aa69
Floreroo <locodediscord@gmail.com>
2021-01-02 02:08:53 +0100

    :3

--- src/commands/Utilidad/user.js
+++ src/commands/Utilidad/user.js
@@ -7,7 +7,7 @@ module.exports = {
     alias: ["u-i", "userinfo"],
     async run (client, message, args) {
 
-    const owo = message.mentions.users.first() || client.users.cache.get(args[0]) || client.users.cache.find(x => x.tag === args[0]) || message.author;
+    const owo = message.mentions.users.first() || client.users.cache.get(args[0]) || client.users.cache.find(x => x.username === args[0]) || client.users.cache.find(x => x.tag === args[0]) || message.author;
     const server = message.guild;
 
     let presencestatus = {
@@ -16,8 +16,8 @@ module.exports = {
     "dnd": "<a:dnd:751961774525251684>| No Molestar",
     "offline": "<a:offline:751961827033874512> | Offline",
     }
- 
- 
+
+
     const badges = {
     "EARLY_SUPPORTER":"<:DiscordNitroEarlySupporter:730664032662585414>",
     "DISCORD_EMPLOYEE": "<:DiscordStaff:730664032419315773>",
@@ -31,21 +31,8 @@ module.exports = {
     "HOUSE_BALANCE": " <:HypeSquadBalance:730664031937101921>",
     "TEAM_USER":"<:Team_User:740667003479785534>",
     "VERIFIED_BOT":"<:Bot:750347018106306590>"
-      
-    }
 
-    var ptext = "";
-    if (owo.presence.activities && owo.presence.activities[0]) {
-    for (const npresence of Object.values(owo.presence.activities)) {
-    if (npresence.type == "CUSTOM_STATUS") {
-    if (npresence.state) ptext +=  npresence.state + " | ";
-    if(npresence.emoji) ptext += npresence.emoji.toString() + " | ";
-    } else {
-    if(npresence.name) ptext += npresence.name + " | ";
     }
-    ptext + "\n";
-    }
-    } else ptext = "No tiene";
 
     const miembro = await message.guild.members.fetch(owo);
 
@@ -59,19 +46,16 @@ module.exports = {
     .addField('Insignias', `${owo.flags.toArray().length < 1 ? `No tiene` : owo.flags.toArray().map(e => badges[e]).join(' ')}`, true)
     .addField('Role destacado', message.guild.member(owo).roles.highest.toString(), true)
     .addField(`Status`,` ${presencestatus[owo.presence.status]}`, true)
-    .addField("Presencia",  ptext.slice(0, 1010) , true)
+    //.addField("Presencia",  ptext.slice(0, 1010) , true)
     .addField(`Ha entrado en ${server.name}`, miembro.joinedAt.toLocaleString("es"), true)
     .addField('Se ha logeado en discord', owo.createdAt.toLocaleString("es"), true)
     .addField('Roles', `${miembro.roles.cache.map(c => c.toString()).join(' | ')}`)
     .setThumbnail(owo.displayAvatarURL({dynamic: true}))
     .setColor('RANDOM')
     message.channel.send(uo)
-  
+
     } catch(err) {
     message.channel.send("Se ha producido un error al buscar este usuario: "+ err)
   }
  }
 }
-
-
-  
