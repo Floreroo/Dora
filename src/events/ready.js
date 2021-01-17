a2f11832eb843a7fa9e27934f6881c1b2972ab07
Floreroo <locodediscord@gmail.com>
2021-01-17 04:58:45 +0100

    🥵
    Co-Authored-By: Melphi <IsMelphi@users.noreply.github.com>

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,8 +5,8 @@ module.exports = async (client) => {
   setInterval(() => {
     client.user.setPresence({
       activity: {
-        name: Fecha(),
-        type: "LISTENING",
+        name: client.ws.ping + "ms",
+        type: "WATCHING",
         $browser: "DISCORD IOS",
         url: "https://twitch.tv/#/"
       },
@@ -15,16 +15,3 @@ module.exports = async (client) => {
     })
   }, 5000)
 }
-function Fecha() {
- var now = new Date();
- var d = now.setDate(now.getUTCDay());
- var mon = now.setMonth(new Date().getMonth());
- var y = now.getFullYear();
- var h = now.getHours();
- var m = now.getMinutes();
- var s = now.getSeconds();
-
- return `${d.toString().substr(0,2)}/${mon.toString().substr(0,1)}/2021 ${h}:${m}:${s}`
-}
-
-Fecha()
