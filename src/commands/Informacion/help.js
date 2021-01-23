95308fc855894606c76dabd058041464ee7c9bd5
Floreroo <locodediscord@gmail.com>
2021-01-23 21:35:22 +0100

    :3
    Co-Authored-By: Melphi <IsMelphi@users.noreply.github.com>

--- src/commands/Informacion/help.js
+++ src/commands/Informacion/help.js
@@ -6,73 +6,8 @@ module.exports = {
   alias: ["comandos"],
   async run(client, message, args, prefix) {
 
-    if (args[0]) {
-
-      if (args[0] == "config") {
-        let embed3 = new Discord.MessageEmbed()
-          .addField("Configuracion", `${prefix}setprefix \n${prefix}setwelcome[${prefix}wel add & remove]`)
-          .setColor('RANDOM')
-          .setFooter('INFORMACION', message.guild.iconURL())
-          .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
-        return message.channel.send(embed3)
-      }
-
-      if (args[0] == "fun") {
-        let embed4 = new Discord.MessageEmbed()
-          .addField("Diversion", `${prefix}8ball \n${prefix}say \n${prefix}embedsay \n${prefix}sayimage \n${prefix}gay \n${prefix}reverse`)
-          .setColor('RANDOM')
-          .setFooter('INFORMACION', message.guild.iconURL())
-          .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
-        return message.channel.send(embed4)
-
-      }
-
-      if (args[0] === "info") {
-        let embed5 = new Discord.MessageEmbed()
-          .addField("Informacion", `${prefix}help \n${prefix}invite \n${prefix}ping \n${prefix}stats`, true)
-          .setColor('RANDOM')
-          .setFooter('INFORMACION', message.guild.iconURL())
-          .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
-        return message.channel.send(embed5)
-
-      }
-
-      if (args[0] == "util") {
-        let embed6 = new Discord.MessageEmbed()
-          .addField("Utilidad", `${prefix}avatar \n${prefix}botinvite \n${prefix}ping  \n${prefix}server \n${prefix}user \n${prefix}channel \n${prefix}role \n${prefix}emoji \n${prefix}rolelist \n${prefix}snipe \n${prefix}icon  \n${prefix}stats`)
-          .setColor('RANDOM')
-          .setFooter('INFORMACION', message.guild.iconURL())
-          .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
-        return message.channel.send(embed6)
-      }
-
-      if (args[0] == "img") {
-        let embed = new Discord.MessageEmbed()
-          .addField("Imagen", `${prefix}pornhub \n${prefix}supreme`)
-            .setColor('RANDOM')
-          .setFooter('INFORMACION', message.guild.iconURL())
-          .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
-        return message.channel.send(embed)
-      }
-
-      if (args[0] == "voice") {
-        let embed0 = new Discord.MessageEmbed()
-          .addField("Voice", `${prefix}join \n${prefix}leave`)
-          .setColor('RANDOM')
-          .setFooter('INFORMACION', message.guild.iconURL())
-          .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
-        return message.channel.send(embed0)
-      }
-
-    if (args[0] == "mod") {
-      let embed1000 = new Discord.MessageEmbed()
-        .addField("Mod", `${prefix}ban \n${prefix}kick`)
-        .setColor('RANDOM')
-        .setFooter('INFORMACION', message.guild.iconURL())
-        .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
-      return message.channel.send(embed1000)
-    }
-    let embed2 = new Discord.MessageEmbed()
+      if (!args[0]){
+      let embed2 = new Discord.MessageEmbed()
       .setThumbnail(client.user.displayAvatarURL({
         dynamic: true,
         size: 2048
@@ -88,7 +23,69 @@ module.exports = {
       .addField("__**Imagen**__", `${prefix}help img`, true)
       .addField("__**Voice**__", `${prefix}help voice`, true)
       .setColor('RANDOM')
-    message.channel.send(embed2)
+    return message.channel.send(embed2)
+    }
+
+switch (args[0]) {
+case 'info':
+ let embed5 = new Discord.MessageEmbed()
+          .addField("Informacion", `${prefix}help \n${prefix}invite \n${prefix}ping \n${prefix}stats`, true)
+          .setColor('RANDOM')
+          .setFooter('INFORMACION', message.guild.iconURL())
+          .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
+       message.channel.send(embed5)
+break;
+case 'config':
+       let embed3 = new Discord.MessageEmbed()
+          .addField("Configuracion", `${prefix}setprefix \n${prefix}setwelcome[${prefix}wel add & remove]`)
+          .setColor('RANDOM')
+          .setFooter('INFORMACION', message.guild.iconURL())
+          .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
+       message.channel.send(embed3)
+break;
+case 'fun':
+let embed4 = new Discord.MessageEmbed()
+          .addField("Diversion", `${prefix}8ball \n${prefix}say \n${prefix}embedsay \n${prefix}sayimage \n${prefix}gay \n${prefix}reverse`)
+          .setColor('RANDOM')
+          .setFooter('INFORMACION', message.guild.iconURL())
+          .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
+       message.channel.send(embed4)
+break;
+case 'util':
+        let embed6 = new Discord.MessageEmbed()
+          .addField("Utilidad", `${prefix}avatar \n${prefix}botinvite \n${prefix}ping  \n${prefix}server \n${prefix}user \n${prefix}channel \n${prefix}role \n${prefix}emoji \n${prefix}rolelist \n${prefix}snipe \n${prefix}icon  \n${prefix}stats`)
+          .setColor('RANDOM')
+          .setFooter('INFORMACION', message.guild.iconURL())
+          .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
+       message.channel.send(embed6)
+break;
+case 'img':
+  let embed = new Discord.MessageEmbed()
+          .addField("Imagen", `${prefix}pornhub \n${prefix}supreme`)
+            .setColor('RANDOM')
+          .setFooter('INFORMACION', message.guild.iconURL())
+          .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
+       message.channel.send(embed)
+break;
+case 'voice':
+ let embed0 = new Discord.MessageEmbed()
+          .addField("Voice", `${prefix}join \n${prefix}leave`)
+          .setColor('RANDOM')
+          .setFooter('INFORMACION', message.guild.iconURL())
+          .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
+        message.channel.send(embed0)
+break;
+  let embed1000 = new Discord.MessageEmbed()
+        .addField("Mod", `${prefix}ban \n${prefix}kick`)
+        .setColor('RANDOM')
+        .setFooter('INFORMACION', message.guild.iconURL())
+        .setAuthor(message.member.user.tag, message.author.displayAvatarURL())
+      message.channel.send(embed1000)
+break;
+  default:
+ message.channel.send("> Esta categoría no existe")
+break;
+};
+
   }
- }
 }
